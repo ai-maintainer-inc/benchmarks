884fb923cbb755352196000d63865101461ff887
David Wurtz <davidjwurtz@gmail.com>
2023-09-05 12:40:17 -0700

    can now build and run Dockerfile

--- python-fastapi-easy-01/.benchmark/eval.sh
+++ python-fastapi-easy-01/.benchmark/eval.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Start the FastAPI app
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 &
+
+# Capture the process ID of the background process so we can terminate it later
+UVICORN_PID=$!
+
+# Wait for the server to start
+sleep 5
+
 # Set default values for HOST and PORT if they are not set
 HOST=${HOST:-localhost}
 PORT=${PORT:-8000}
@@ -30,6 +39,9 @@ else
   echo "Failure: /ping endpoint does not contain 'pong'"
   STATUS=1
 fi
+
+# Terminate the uvicorn process
+kill $UVICORN_PID
 
 # Return the overall status
 exit $STATUS
